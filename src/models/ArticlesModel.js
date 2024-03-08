428045f6b7623fd779df68ea7507ba7fe3450d29
Isramss <isra.msadak@gmail.com>
2024-03-08 12:42:57 +0100

    Creation des modeles User et Articles

---
+++ src/models/ArticlesModel.js
@@ -0,0 +1,20 @@
+import { mongoose, Schema } from "mongoose";
+
+const articleSchema = new Schema(
+  {
+    title_Produit: { type: String, required: true, unique: true },
+    imageURL: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+    description: { type: String },
+  },
+  { timestamps: true }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+
+export default Article;
